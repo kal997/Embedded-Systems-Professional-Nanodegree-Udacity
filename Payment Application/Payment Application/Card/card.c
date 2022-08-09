479d8ff8ce0898aeffd4da0a17673ee43f206507
Khaled Soliman <Khaled.soliman97@gmail.com>
2022-08-09 20:34:17 +0200

    Card module, getCardHolderName function is done and tested(it can be improved)

--- Payment Application/Payment Application/Card/card.c
+++ Payment Application/Payment Application/Card/card.c
@@ -1,3 +1,49 @@
 #include<stdio.h>
 #include<string.h>
 #include "card.h"
+
+
+
+
+
+EN_cardError_t getCardHolderName(ST_cardData_t* cardData)
+{
+	EN_cardError_t err;
+	uint8_t enteredName[30];
+	uint8_t enteredNameLen;
+	printf("Please Enter Card Holder Name: ");
+	gets(enteredName);
+	enteredNameLen = strlen(enteredName);
+
+	if (enteredNameLen == 0 || enteredNameLen > 24)
+	{
+		err = WRONG_NAME;
+		return err;
+	}
+	else
+	{
+		stringCopy(enteredName, cardData->cardHolderName);
+		err = OK;
+		return err;
+	}
+
+}
+
+
+EN_cardError_t getCardExpiryDate(ST_cardData_t* cardData)
+{
+
+}
+
+
+EN_cardError_t getCardPAN(ST_cardData_t* cardData)
+{
+
+}
+
+void stringCopy(uint8_t* src, uint8_t* dest)
+{
+	uint8_t i;
+	for (i = 0; src[i] != '\0'; i++) dest[i] = src[i];
+	dest[i] = '\0';
+}
--- Payment Application/Payment Application/Card/card.h
+++ Payment Application/Payment Application/Card/card.h
@@ -1,10 +1,26 @@
 #ifndef CARD_H
 #define CARD_H
 
+#include "../standard_types.h"
 
 
+typedef struct ST_cardData_t
+{
+	uint8_t cardHolderName[25];
+	uint8_t primaryAccountNumber[20];
+	uint8_t cardExpirationDate[6];
+}ST_cardData_t;
+
+typedef enum EN_cardError_t
+{
+	OK, WRONG_NAME, WRONG_EXP_DATE, WRONG_PAN
+}EN_cardError_t;
 
 
+EN_cardError_t getCardHolderName(ST_cardData_t* cardData);
+EN_cardError_t getCardExpiryDate(ST_cardData_t* cardData);
+EN_cardError_t getCardPAN(ST_cardData_t* cardData);
+void stringCopy(uint8_t*, uint8_t*);
 
 
 #endif // !CARD_H
