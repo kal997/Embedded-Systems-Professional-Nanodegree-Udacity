309acfa0c43ec5bac3a3a2ff690b1bd8eadb295c
Khaled Soliman <Khaled.soliman97@gmail.com>
2022-08-09 20:24:29 +0200

    adds header guards, and edits standard types

--- Payment Application/Payment Application/standard_types.h
+++ Payment Application/Payment Application/standard_types.h
@@ -1,6 +1,20 @@
 #ifndef STANDARD_TYPES_H
 #define STANDARD_TYPES_H
 
+typedef unsigned char uint8_t;
+typedef signed char int8_t;
+
+typedef unsigned short uint16_t;
+typedef signed short int16_t;
+
+typedef unsigned long uint32_t;
+typedef signed long int32_t;
+
+typedef unsigned long long uint64_t;
+typedef signed long long int64_t;
+
+#define NULLPTR ((void*)0)
+
 
 
 #endif // !STANDARD_TYPES_H
