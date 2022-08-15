961595b0b30c6af4840e6f514ce30d2cfdd36b9b
Khaled Soliman <Khaled.soliman97@gmail.com>
2022-08-15 15:42:25 +0200

    isBelowMaxAmount is done and tested

--- Payment Application/Payment Application/Terminal/terminal.c
+++ Payment Application/Payment Application/Terminal/terminal.c
@@ -106,7 +106,11 @@ EN_terminalError_t getTransactionAmount(ST_terminalData_t* termData)
 	return TERMINAL_OK;
 
 }
-EN_terminalError_t isBelowMaxAmount(ST_terminalData_t* termData);
+EN_terminalError_t isBelowMaxAmount(ST_terminalData_t* termData)
+{
+	
+	return termData->transAmount > termData->maxTransAmount ? EXCEED_MAX_AMOUNT : TERMINAL_OK;
+}
 EN_terminalError_t setMaxAmount(ST_terminalData_t* termData)
 {
 	float maxTransactionAmount;
