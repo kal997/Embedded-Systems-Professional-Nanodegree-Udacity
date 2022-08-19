fa7f8c5300fc3fdabe9349974622dc7e03e3d805
Khaled Soliman <Khaled.soliman97@gmail.com>
2022-08-19 15:37:04 +0200

    (creates accountIdx static global to share the found account between isValidAccount and isAmountAvailable(I'll search for better solution))---- isAmountAvailable is done and tested

--- Payment Application/Payment Application/Server/server.c
+++ Payment Application/Payment Application/Server/server.c
@@ -1,23 +1,40 @@
 #include<stdio.h>
 #include"server.h"
 
+// -1 (not found), any non-negative(found)
+static int16_t accountIDx = -1;
+
 
 static ST_accountsDB_t accountsDB[4] = { { 15000.5, "27983973780030443" }, {20500.2, "5261716147787190"}, {1456.6, "5261716147787190"}, {14590.352, "2170049069582404320"}};
 static ST_transaction_t transactionDB[255] = { 0 };
 static uint8_t transactionDBIdx = 0;
 
 
-/*
-This function will take card data and validate these data.
-It checks if the PAN exists or not in the server's database.
-If the PAN doesn't exist will return DECLINED_STOLEN_CARD, else will return OK
-*/
+
 EN_serverError_t isValidAccount(ST_cardData_t* cardData)
 {
 	uint8_t i;
 	for (i = 0; i < sizeof(accountsDB) / sizeof(accountsDB[0]); i++)
 	{
-		if (!strcmp(cardData->primaryAccountNumber ,accountsDB[i].primaryAccountNumber)) return SERVER_OK;
+		if (!strcmp(cardData->primaryAccountNumber, accountsDB[i].primaryAccountNumber))
+		{
+			accountIDx = i;
+			return SERVER_OK;
+		}
 	}
 	return ACCOUNT_NOT_FOUND;
+}
+
+EN_serverError_t isAmountAvailable(ST_terminalData_t* termData)
+{
+	if (termData->transAmount > accountsDB[accountIDx].balance)
+	{
+		printf("\n   %f\n", accountsDB[accountIDx].balance);
+		return LOW_BALANCE;
+	}
+	printf("\n   %f\n", accountsDB[accountIDx].balance);
+	return SERVER_OK;
+	
+	
+
 }
--- Payment Application/Payment Application/Server/server.h
+++ Payment Application/Payment Application/Server/server.h
@@ -30,12 +30,10 @@ typedef struct ST_accountsDB_t
 	uint8_t primaryAccountNumber[20];
 }ST_accountsDB_t;
 
-
 EN_transState_t recieveTransactionData(ST_transaction_t* transData);
 EN_serverError_t isValidAccount(ST_cardData_t* cardData);
+EN_serverError_t isAmountAvailable(ST_terminalData_t* termData);
 EN_serverError_t saveTransaction(ST_transaction_t* transData);
 EN_serverError_t getTransaction(uint32_t transactionSequenceNumber, ST_transaction_t* transData);
-
-
 
 #endif // ! SERVER_H
